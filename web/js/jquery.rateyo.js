7cd0fa61c907f27a65d583a3b20d882c3448c348
pmary <pmary@gecko-btl.com>
2016-08-31 14:11:26 -0500

    change STARS BETA

--- web/js/jquery.rateyo.js
+++ web/js/jquery.rateyo.js
@@ -6,21 +6,23 @@
 
 ;(function ($) {
   "use strict";
-
+  /*
+   <svg version=\"1.1\""+
+   "xmlns=\"http://www.w3.org/2000/svg\""+
+   "viewBox=\"0 12.705 512 486.59\""+
+   "x=\"0px\" y=\"0px\""+
+   "xml:space=\"preserve\">"+
+   "<polygon "+
+   "points=\"256.814,12.705 317.205,198.566"+
+   " 512.631,198.566 354.529,313.435 "+
+   "414.918,499.295 256.814,384.427 "+
+   "98.713,499.295 159.102,313.435 "+
+   "1,198.566 196.426,198.566 \"/>"+
+   "</svg>
+   */
   // The basic svg string required to generate stars
   var BASICSTAR = "<?xml version=\"1.0\" encoding=\"utf-8\"?>"+
-                  "<svg version=\"1.1\""+
-                        "xmlns=\"http://www.w3.org/2000/svg\""+
-                        "viewBox=\"0 12.705 512 486.59\""+
-                        "x=\"0px\" y=\"0px\""+
-                        "xml:space=\"preserve\">"+
-                    "<polygon "+
-                              "points=\"256.814,12.705 317.205,198.566"+
-                                      " 512.631,198.566 354.529,313.435 "+
-                                      "414.918,499.295 256.814,384.427 "+
-                                      "98.713,499.295 159.102,313.435 "+
-                                      "1,198.566 196.426,198.566 \"/>"+
-                  "</svg>";
+                  '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"/></svg>';
 
   // The Default values of different options available in the Plugin
   var DEFAULTS = {
